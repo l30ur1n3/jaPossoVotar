5a0edbd4f2d7e2dd07b348f640d20afb3fcd2736
Rodrigo Bernardo Torres <162391152+l30ur1n3@users.noreply.github.com>
2024-05-06 15:35:49 -0300

    Add files via upload
    Programa em Java POO criado por mim para treino

---
+++ UltraEmojiCombat/src/main/java/com/mycompany/ultraemojicombat/Luta.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ultraemojicombat;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Luta {
+    //Atributos
+    private Lutador desafiado;
+    private Lutador desafiante;
+    private int rounds;
+    private boolean aprovada;
+    //Métodos publicos
+    public void marcarLuta(Lutador l1,Lutador l2){
+        if(l1.getCategoria().equals(l2.getCategoria()) && l1 != l2){
+            this.aprovada = true;
+            this.desafiado = l1;
+            this.desafiante = l2;
+        }else{
+            this.aprovada = false;
+            this.desafiado = null;
+            this.desafiante = null;
+        }
+    }
+    public void lutar(){
+        if(this.aprovada){
+            System.out.println("#####DESAFIADO###");
+            this.desafiado.apresentar();
+            System.out.println("#####DESAFIANTE###");
+            this.desafiante.apresentar();
+            
+            Random aleatorio = new Random();
+            int vencedor = aleatorio.nextInt(3);
+            switch(vencedor){
+                case 0: //Empate
+                    System.out.println("Empatou!");
+                    this.desafiado.empatarLuta();
+                    this.desafiante.empatarLuta();
+                    break;
+                case 1: 
+                    System.out.println("Venceu " + this.desafiado.getNome());
+                    this.desafiado.ganharLuta();
+                    this.desafiante.perderLuta();
+                    break;
+                case 2:
+                    System.out.println("Venceu " + this.desafiante.getNome());
+                    this.desafiado.perderLuta();
+                    this.desafiante.ganharLuta();
+                    break;
+                
+            }
+            //Empate
+            // Desafiado vence
+            //Desafiante vence
+                        
+            
+        }else{
+            System.out.println("A luta não pode acontecer");
+        }
+    }
+    //Métodos especiais
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+    
+    
+}
